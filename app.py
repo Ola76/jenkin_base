fdbad9bff4f4c2795787c66ef64f2f440abf9a6b
Ola76 <Oritsebamigho.oburoh@cbs-mail.de>
2023-10-30 13:53:54 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+st.header("Welcome to my new App")
+
+st.write("This is the testing of the Dockerfile and Streamlit to build CI/CD on Jenkins")
+
+st.write("I want to make the project connect to my jenkins server")
